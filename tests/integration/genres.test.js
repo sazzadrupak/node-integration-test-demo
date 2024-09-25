c59bce36ca6e2462d8baa0628a9dd2b852b8e6e0
sazzadrupak <sazzad@oployeelabs.com>
2024-09-25 20:13:00 +0300

    Testing the authorization

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -45,4 +45,14 @@ describe('/api/genres', () => {
       expect(res.status).toBe(404);
     });
   });
+
+  describe('POST /', () => {
+    it('should return 401 if client is not logged in', async () => {
+      const res = await request(server)
+        .post('/api/genre')
+        .send({ name: 'genre1' });
+
+      expect(res.status).toBe(401);
+    });
+  });
 });
