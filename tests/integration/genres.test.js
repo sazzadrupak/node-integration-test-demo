3be692e18c614073f18c40697246b1439a482a32
sazzadrupak <sazzad@oployeelabs.com>
2024-09-25 19:52:44 +0300

    Test /api/genre/:id get method

--- tests/integration/genres.test.js
+++ tests/integration/genres.test.js
@@ -27,4 +27,16 @@ describe('/api/genres', () => {
       expect(res.body.some((g) => g.name === 'genre2')).toBeTruthy();
     });
   });
+
+  describe('GET /:id', () => {
+    it('should return genre if valid is passed', async () => {
+      const genre = new Genre({ name: 'genre1' });
+      await genre.save();
+
+      const res = await request(server).get(`/api/genres/${genre._id}`);
+
+      expect(res.status).toBe(200);
+      expect(res.body).toHaveProperty('name', genre.name);
+    });
+  });
 });
