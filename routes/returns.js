a842d8f01b145e34dff53172a5739539e8483165
sazzadrupak <sazzad@oployeelabs.com>
2024-09-26 12:00:30 +0300

    Refactoring the validation

--- routes/returns.js
+++ routes/returns.js
@@ -1,20 +1,14 @@
 const { Rental } = require('../models/rental');
 const { Movie } = require('../models/movie');
 const auth = require('../middleware/auth');
+const validate = require('../middleware/validate');
 
+const Joi = require('joi');
 const moment = require('moment');
 const express = require('express');
 const router = express.Router();
 
-router.post('/', auth, async (req, res) => {
-  if (!req.body.customerId) {
-    return res.status(400).send('customerId not provided');
-  }
-
-  if (!req.body.movieId) {
-    return res.status(400).send('movieId not provided');
-  }
-
+router.post('/', [auth, validate(validateReturn)], async (req, res) => {
   const rental = Rental.findOne({
     'customer._id': req.body.customerId,
     'movie._id': req.body.movieId,
@@ -41,5 +35,13 @@ router.post('/', auth, async (req, res) => {
 
   return res.status(200).send(rental);
 });
+
+function validateReturn(req) {
+  const schema = {
+    customerId: Joi.objectId().required(),
+    movieId: Joi.objectId().required(),
+  };
+  return Joi.validate(req, schema);
+}
 
 module.exports = router;
